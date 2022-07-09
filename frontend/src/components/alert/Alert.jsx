46e3c2d8cb9de2035bf407ce05335a9f3a984be2
Dayvyno2021 <dayvynomern2021@gmail.com>
2022-07-09 08:18:28 +0100

    removed build file from gitignore

--- frontend/src/components/alert/Alert.jsx
+++ frontend/src/components/alert/Alert.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
 function Alert({message}){
-  // const notify = () => toast(message);
-  // const notify = () => toast("Wow so easy!");
   return (
     <div>
       {alert(message)}
-      {/* <button onClick={notify}>Notify!</button>
-      <ToastContainer /> */}
-
     </div>
   );
+}
+
+Alert.prototype={
+  message: "Error Occured"
 }
 export default Alert
